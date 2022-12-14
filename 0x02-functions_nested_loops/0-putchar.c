7dfd000398af7db28977e940ac29708a7a45bb44
Chukzyfrosh <chukzyfrosh@gmail.com>
2022-12-13 18:54:59 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - prints _putchar and a new line
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
-
 {
 	_putchar('_');
-	_putchar('P');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
