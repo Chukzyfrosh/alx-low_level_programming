92ff9eae6373c9d4aecec711dd77a7a9eb013c28
Chukzyfrosh <chukzyfrosh@gmail.com>
2022-12-15 17:04:24 -0800

    largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,8 +9,8 @@
  */
 
 int largest_number(int a, int b, int c)
-
 {
+
 	int largest;
 
 	if (a >= b && a >= c)
@@ -26,5 +26,4 @@ int largest_number(int a, int b, int c)
 	largest = c;
 	}
 	return (largest);
-	}
 }
