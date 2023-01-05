84b6460d4884d372019b9c70d9d6af0585a69539
Chukzyfrosh <chukzyfrosh@gmail.com>
2023-01-04 17:59:11 -0800

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+/*
+ * 3-factorial.c
+ * Auth: Chukzy
+ */
+
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
+ *
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n == 0) /*Base condition*/
+
+		return (1);
+
+	else
+	if (n < 0) /*Base condition*/
+		return (-1);
+	else
+		return (n * factorial(n - 1)); /*Recursive call*/
+}
