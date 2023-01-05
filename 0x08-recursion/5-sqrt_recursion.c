19cfbac067934ccbb213a0e267dfa0291037d61a
Chukzyfrosh <chukzyfrosh@gmail.com>
2023-01-04 20:17:00 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+/*
+ * File - 5-sqrt_recursion.c
+ * Auth: Chukzy
+ */
+
+#include "main.h"
+
+/**
+ * _evaluate - evaluate function sqrt
+ * @i: integer
+ * @n: integer
+ * Return: evaluate sqrt
+ */
+
+int _evaluate(int i, int n)
+{
+	/*Evaluate function*/
+	if (n == 0 ||  == 1)
+		return (n);
+
+	else
+	if (i * i < n)
+		return (_evalute(i +, n));
+
+	else
+	if (i * i == n) /*condition base*/
+		return (i);
+
+	return (-1);
+
+	return (-1);
+}
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integer
+ * Return: sqrt_recursion
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0) /*if n is negative*/
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n)); /*Recursive call*/
+	}
+}
